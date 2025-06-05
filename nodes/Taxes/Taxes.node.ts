31eba8aadc7ac9e79aa31875d21154bcc708ca05
Altazion Robot <services@altazion.com>
2025-06-05 23:13:35 +0200

    Updated generated code on 2025-06-05 23:13:35

---
+++ nodes/Taxes/Taxes.node.ts
@@ -0,0 +1,53 @@
+import { INodeType, INodeTypeDescription } from 'n8n-workflow';
+import { resTaxes  } from './Taxes';
+import { resVat  } from './Vat';
+
+export class Taxes implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Altazion API - Taxes',
+		name: 'Taxes',
+		icon: {light: 'file:altazion-noir.svg', 
+			dark: 'file:altazion-blanc.svg' 
+		},
+		group: ['transform'],
+		version: 1,
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		description: 'Interact Altazion API',
+		defaults: {
+			name: 'Altazion Taxes',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'altazionApi',
+				required: true,
+			},
+		],
+		requestDefaults: {
+			baseURL: 'https://app.office.altazion.com',
+			url: '',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		},
+		properties: [
+{
+		displayName: 'Resource',
+		name: 'resource',
+		type: 'options',
+		noDataExpression: true,
+		options: [
+			{ name: 'Taxes', value: 'Taxes' },
+			{ name: 'Vat', value: 'VAT' },
+		],
+		default: 'Taxes',
+	},
+			 ...resTaxes,
+			 ...resVat,
+			
+
+		]
+	};
+}
